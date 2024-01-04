cc84c2efcd1d4406e047e7a69290e60dee9d710a
rexong <rexonghanwei200035@gmail.com>
2024-01-04 17:50:39 +0800

    feat: Set up database and model for user

---
+++ backend/src/main.py
@@ -0,0 +1,24 @@
+from fastapi import FastAPI, status, Depends, HTTPException
+import models
+from database import engine, SessionLocal
+from typing import Annotated
+from sqlalchemy.orm import Session
+
+app = FastAPI()
+
+models.Base.metadata.create_all(bind=engine)
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+db_dependency = Annotated[Session, Depends(get_db)]
+
+@app.get('/', status_code = status.HTTP_200_OK)
+def user(user: None, db: db_dependency):
+    if not user:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Authentication Failed")
+    return {"User": user}
