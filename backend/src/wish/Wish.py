ac6f3c0799d47eda1c7b29e4fe264bcdff9e3835
rexong <rexonghanwei200035@gmail.com>
2024-01-06 02:25:19 +0800

    feat: Implement hide and unhide wish

--- backend/src/wish/Wish.py
+++ backend/src/wish/Wish.py
@@ -3,7 +3,7 @@ from typing import Annotated
 from fastapi import APIRouter, Depends, status
 from sqlalchemy.orm import Session
 
-from .services import add_new_wish_to_db, get_user_wishes_from_db, remove_wish_from_db
+from .services import add_new_wish_to_db, get_user_wishes_from_db, hide_wish_in_db, remove_wish_from_db, unhide_wish_in_db
 from ..auth.services import get_current_user
 from ..database import Get_DB
 from .. import schemas
@@ -28,14 +28,18 @@ def read_wishes(user: user_dependency, db: db_dependency):
 def create_wishes(db: db_dependency, wish: schemas.WishCreate, user: user_dependency):
     return add_new_wish_to_db(**wish.model_dump(), db=db, owner_id=user.id)
 
-@router.delete('/{id}', dependencies=[Depends(get_current_user)])
-def delete_wishes(db: db_dependency, id: int):
-    return remove_wish_from_db(db=db, id=id)
+@router.delete('/{id}')
+def delete_wishes(db: db_dependency, id: int, user: user_dependency):
+    return remove_wish_from_db(db=db, id=id, user_id=user.id)
 
-@router.put('')
-def update_wishes():
+@router.put('/{id}')
+def update_wishes(db: db_dependency, user: user_dependency, wish: schemas.WishCreate):
     pass
 
-@router.put('')
-def toggle_is_hidden():
-    pass
+@router.put('/{id}/hide', response_model=schemas.Wish)
+def hide_wish(db: db_dependency, user: user_dependency, id: int):
+    return hide_wish_in_db(db=db, id=id, user_id=user.id)
+
+@router.put('/{id}/unhide', response_model=schemas.Wish)
+def unhide_wish(db: db_dependency, user: user_dependency, id: int):
+    return unhide_wish_in_db(db=db, id=id, user_id=user.id)
--- backend/src/wish/services.py
+++ backend/src/wish/services.py
@@ -4,6 +4,9 @@ from sqlalchemy.orm import Session
 from .. import models
 from ..auth.services import get_user_from_db_by_id, USER_NOT_FOUND_EXCEPTION
 
+WISH_NOT_FOUND_EXCEPTION = HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Wish Not Found")
+WISH_OPERATION_FORBIDDEN_EXCEPTION = HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="No Permission to Edit this Wish")
+
 def add_new_wish_to_db(
     title: str,
     db: Session,
@@ -38,10 +41,36 @@ def get_user_wishes_from_db(
 def get_wish_from_db_by_id(db: Session, id: int) -> models.Wish | None:
     return db.query(models.Wish).filter(models.Wish.id == id).first()
 
-def remove_wish_from_db(db: Session, id: int):
+def remove_wish_from_db(db: Session, id: int, user_id: int):
     wishDB = get_wish_from_db_by_id(db, id)
     if not wishDB:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Wish Not Found")
+        raise WISH_NOT_FOUND_EXCEPTION
+    if wishDB.owner_id != user_id:
+        raise WISH_OPERATION_FORBIDDEN_EXCEPTION
     db.delete(wishDB)
+    db.commit()
+    return wishDB
+
+def hide_wish_in_db(db: Session, id: int, user_id: int):
+    wishDB = get_wish_from_db_by_id(db, id)
+    if not wishDB:
+        raise WISH_NOT_FOUND_EXCEPTION
+    if wishDB.owner_id != user_id:
+        raise WISH_OPERATION_FORBIDDEN_EXCEPTION
+    if wishDB.is_hidden == True:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Wish is already Hidden")
+    wishDB.is_hidden = True
+    db.commit()
+    return wishDB
+
+def unhide_wish_in_db(db: Session,id: int, user_id: int):
+    wishDB = get_wish_from_db_by_id(db, id)
+    if not wishDB:
+        raise WISH_NOT_FOUND_EXCEPTION
+    if wishDB.owner_id != user_id:
+        raise WISH_OPERATION_FORBIDDEN_EXCEPTION
+    if wishDB.is_hidden == False:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Wish is already Unhidden")
+    wishDB.is_hidden = False
     db.commit()
     return wishDB
