0e505d53de81e2a34acba3e73c45c44c09c630d5
rexong <rexonghanwei200035@gmail.com>
2024-01-05 22:30:30 +0800

    fix: Check if user exist in db

--- backend/src/main.py
+++ backend/src/main.py
@@ -4,7 +4,11 @@ from sqlalchemy.orm import Session
 
 from . import models, schemas
 from .auth import Auth
-from .auth.services import get_current_user
+from .auth.services import (
+   USER_NOT_FOUND_EXCEPTION,
+   get_current_user,
+   get_user_from_db_by_email 
+)
 from .database import engine, Get_DB
 
 app = FastAPI()
@@ -17,4 +21,7 @@ user_dependency = Annotated[schemas.User, Depends(get_current_user)]
 
 @app.get('/me', status_code = status.HTTP_200_OK, response_model=schemas.User)
 def user(user: user_dependency, db: db_dependency):
+    userDB = get_user_from_db_by_email(user.email, db)
+    if not userDB:
+        raise USER_NOT_FOUND_EXCEPTION
     return user
