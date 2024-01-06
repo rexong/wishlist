5b33ced50544f26d4954215270cd6ea786fba59a
rexong <rexonghanwei200035@gmail.com>
2024-01-06 13:20:21 +0800

    chore: Fix Lint

--- backend/src/auth/Auth.py
+++ backend/src/auth/Auth.py
@@ -30,7 +30,6 @@ def create_user(db: db_dependency, user: schemas.UserCreate):
 def delete_user(db: db_dependency, user: user_dependency):
     return delete_user_from_db(user.email, db)
 
-
 @router.post('/token', response_model=schemas.Token)
 def login_for_access_token(form_data: Annotated[OAuth2PasswordRequestForm, Depends()], db: db_dependency):
     user = authenticate_user(form_data.username, form_data.password, db)
